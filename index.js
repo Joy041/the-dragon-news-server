a1412d26747681d4c7c747c87634cff57a15f8e3
Joy Purkistha <joypurokisthosh@gmail.com>
2023-05-01 03:50:49 +0600

    create new routes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const categories = require('./data/Categories.json')
 const port = 5000;
+
+const categories = require('./data/Categories.json')
+const news = require('./data/news.json')
 
 app.use(cors())
 
@@ -10,12 +12,30 @@ app.get('/', (req, res) => {
     res.send('the dragon news')
 })
 
-
 app.get('/categories', (req, res) => {
     res.send(categories)
 })
 
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+        res.send(categoryNews)
+    }
+})
 
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const newsId = news.find(n => n._id === id)
+    res.send(newsId)
+})
 
 app.listen(port, () => {
     console.log(`listening on port = ${port}`)
